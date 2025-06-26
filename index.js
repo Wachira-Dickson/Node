387efe633c1d7c94582b3254da3c0aa5570d9c43
Your Name <dicksonwachira886@gmail.com>
2025-06-26 18:15:34 +0300

    Running Javasvript files with Node JS

---
+++ index.js
@@ -0,0 +1,11 @@
+console.log('Hello, World!');
+
+const array = [1, 2, 3, 4, 5];
+
+console.log(array, 'array');
+
+setTimeout(() => {
+  console.log('This is a delayed message');
+}, 2000);
+
+console.log('This is the last sync message');
