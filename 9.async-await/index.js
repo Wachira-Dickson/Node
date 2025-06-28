77247f3e92eebb9af8b1022c5cacfc9f7c7c301a
Your Name <dicksonwachira886@gmail.com>
2025-06-28 20:09:51 +0300

    Add async delayed greeting and safe division functions with error handling

---
+++ 9.async-await/index.js
@@ -0,0 +1,27 @@
+function delayFn(time) {
+    return new Promise((resolve) => setTimeout(resolve, time));
+}
+
+async function delayedGreet (name) {
+    await delayFn(2000);
+    console.log(name)
+}
+
+delayedGreet("Dickson");
+
+async function division(num1, num2){
+    try{
+        if(num2 === 0) throw new Error('Can not perform division by zero');
+        return num1 / num2;
+    }catch(error){
+        console.error('Error:', error);
+        return null;
+    }
+}
+
+async function mainFn(){
+    console.log(await division(10, 2));
+    console.log(await division(10, 0));
+}
+
+mainFn();
